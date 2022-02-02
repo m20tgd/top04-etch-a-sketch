6278f6aa57d7f510e2775c7b017ee81b0ac60fd2
Tom Digby <m20tgd@hotmail.co.uk>
2022-02-02 16:29:20 +0000

    Remove alpha value from multicoloured mode that was added for debugging.

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function getColour(currentColour){
             const red = Math.floor(Math.random() * 256);
         const green = Math.floor(Math.random() * 256);
         const blue = Math.floor(Math.random() * 256);
-        return (`rgba(${red}, ${green}, ${blue}, .5)`);
+        return (`rgba(${red}, ${green}, ${blue})`);
     }
 }
 
